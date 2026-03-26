13869340c5355a12d913c717bdbb5e17644f11c4
theriakhurana <theriakhurana@gmail.com>
2026-03-26 22:25:01 +0530

    feat(server): added JWT-based authentication with token generation and verification

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userModel from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 
 const router = express.Router();
 
@@ -10,18 +11,68 @@ router.post("/register", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
     password = await bcrypt.hash(password, salt);
+    
+    let user = await userModel.create({
+      username,
+      email,
+      password,
+      role,
+    });
+    
+    const payload = {
+      id: user._id,
+      role: user.role,
+    };
+    
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
+    res.cookie(token, {token});
+    
+    res.json(token);
+
   } catch (err) {
-    throw err;
+    console.log(err);
+    return res.status(500).json({msg: "Server error"});
   }
-
-  let newUser = await userModel.create({
-    username,
-    email,
-    password,
-    role,
-  });
-
-  res.send(newUser);
 });
 
+router.post("/login", async (req, res)=>{
+  let {email, password} = req.body;
+
+  try{
+    let user = await userModel.findOne({email});
+    if (!user) {
+      return res.status(400).json({msg : "Invalid Credentials" });
+    }
+    const matched = await bcrypt.compare(password, user.password);
+    if (!matched) {
+      return res.status(400).json({msg : "Invalid Credentials"});
+    }
+  
+    const payload = {
+      id: user._id,
+      role: user.role,
+    };
+  
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
+    res.cookie(token, {token});
+
+    res.json(token);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({msg: "Server error"});
+  }
+
+})
+
+router.get("/logout", async (req, res)=>{
+  res.cookie(token, "");
+  res.redirect("/");
+})
+
 export default router;
+
+// const token = jwt.sign(
+//     { id: user._id, role: user.role },
+//     process.env.JWT_SECRET,
+//     { expiresIn: '7d' }
+// );
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("API run ho rhi hai :))");
